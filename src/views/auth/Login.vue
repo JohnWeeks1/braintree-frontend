b69b8fee08e59bcf29ea882c2af4e3aea3d634a9
JohnWeeks1 <jonathanweeks007@gmail.com>
2020-10-25 22:09:25 +0000

    Changed color of text in button

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -18,7 +18,7 @@
                                 {{ errors.password[0] }}</p>
                         </div>
                         <div class="flex items-center justify-between">
-                            <button @click="login" class="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded focus:outline-none
+                            <button @click="login" class="bg-blue-500 text-white hover:bg-blue-700 font-bold py-2 px-4 rounded focus:outline-none
                                 focus:shadow-outline" type="button">
                                 Sign In
                             </button>
