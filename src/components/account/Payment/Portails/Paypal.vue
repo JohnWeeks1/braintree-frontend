c62f54d16f8b84a6b2ca2f9cd45f1b725af2aac2
JohnWeeks1 <jonathanweeks007@gmail.com>
2020-10-29 22:34:10 +0000

    Added const self = this in PayPal component

--- src/components/account/Payment/Portails/Paypal.vue
+++ src/components/account/Payment/Portails/Paypal.vue
@@ -14,7 +14,8 @@ export default {
     name: 'Paypal',
     data() {
         return {
-            error: null
+            error: null,
+            amount: 10.00
         }
     },
     mounted() {
@@ -25,57 +26,68 @@ export default {
          * Initialize Paypal
          */
         initPaypal() {
+            const self = this;
+
             braintree.client.create({
                 authorization: process.env.VUE_APP_BRAINTREE_SANDBOX_KEY
-            }).then(function (clientInstance) {
+            }).then(clientInstance => {
                 return braintree.paypalCheckout.create({
                     client: clientInstance
                 });
-            }).then(function (paypalInstance) {
-                return paypal.Button.render({
+            }).then(paypalInstance => {
+                return paypal.Button.render( {
                     env: 'sandbox',
                     style: {
                         label: 'paypal',
-                        size: 'responsive',
+                        size : 'responsive',
                         shape: 'rect'
                     },
                     payment() {
                         return paypalInstance.createPayment({
                             flow: 'checkout',
                             intent: 'sale',
-                            amount: 10.00,
+                            amount: self.amount,
                             displayName: 'Braintree Testing',
-                            currency: 'USD'
+                            currency: 'USD',
                         })
                     },
                     onAuthorize(data, options) {
                         return paypalInstance.tokenizePayment(options)
                             .then(response => {
-                            console.log(response);
-                            // this.error = "";
-                            axios.post("api/payments", {
-                                nonce: response.nonce,
-                                amount: 10.00,
-                            }).then(() => {
-                                    this.success = true;
-                            }).catch(error => {
-                                console.log(error);
-                            })
+                                self.error = null;
+                                self.submitPayment(response);
                         })
                     },
-                    // onCancel: (data) => {
-                    //     console.log(data);
-                    //     console.log("Payment Cancelled");
-                    // },
-                    // onError: (err) => {
-                    //     console.error(err);
-                    //     this.error = "An error occurred while processing the paypal payment.";
-                    // }
+                    onCancel(data) {
+                        console.log(data);
+                        console.log("Payment Cancelled");
+                    },
+                    onError(err) {
+                        console.error(err);
+                        self.error = "An error occurred while processing the paypal payment.";
+                    }
                 }, '#paypal-button')
             }).catch(function (err) {
                 console.error('Error!', err);
             });
         },
+
+        /**
+         * Submit payment.
+         *
+         * @param response
+         */
+        submitPayment(response) {
+            axios.post("api/payments", {
+                nonce: response.nonce,
+                amount: this.amount,
+            }).then(() => {
+                this.success = true;
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+
     },
 }
 </script>
--- src/components/partials/alerts/DangerAlert.vue
+++ src/components/partials/alerts/DangerAlert.vue
@@ -14,7 +14,7 @@ export default {
     props: {
         error: {
             type: Object,
-            required: false,
+            required: false
         }
     }
 };
